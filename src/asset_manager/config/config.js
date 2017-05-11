5f732f568d47602a5e1102851fe8edcafc329d3a
Mark Murphy <markmurphy@Marks-MacBook-Pro.local>
2017-05-11 16:47:03 -0230

    initial upload change

--- src/asset_manager/config/config.js
+++ src/asset_manager/config/config.js
@@ -7,7 +7,10 @@ define(function () {
 		stylePrefix: 'am-',
 
 		// Url where uploads will be send, set false to disable upload
-		upload: 'http://localhost/assets/upload',
+		upload: 'http://localhost:8081/',//'http://localhost/assets/upload',
+
+		// Upload auth
+		token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTcxMDYwNjk4MTh9.Q1aPJAzWLLADKpCwP85dsTXnhzJdVZkuEbDxsWKWdiM',
 
 		// Text on upload input
 		uploadText: 'Drop files here or click to upload',
